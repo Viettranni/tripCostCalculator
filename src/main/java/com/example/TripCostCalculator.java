938973dea9b142315a995913128decf6d30e9252
viettranni <vttranviett@gmail.com>
2025-03-06 13:52:13 +0200

    Initial push

---
+++ src/main/java/com/example/TripCostCalculator.java
@@ -0,0 +1,37 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class TripCostCalculator {
+    
+    public static double calculateTotalCost(double travelDistance, double pricePerLiter, double fuelConsumption) {
+        double totalFuelNeeded = (travelDistance / 100) * fuelConsumption;
+
+        double totalCost = totalFuelNeeded * pricePerLiter;
+        return totalCost;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Fixed fuel consumption
+        double fuelConsumption = 5;
+
+        // Travel Distance
+        System.out.println("What is your travel distance (km)? ");
+        double travelDistance = scanner.nextDouble();
+
+        // Fuel Price per Liter
+        System.out.println("What is the Fuel Price per Liter (€)? ");
+        double pricePerLiter = scanner.nextDouble();
+
+        double totalCost = calculateTotalCost(travelDistance, pricePerLiter, fuelConsumption);
+
+        System.out.println("The total price for this Trip is: " + totalCost + " €!");
+
+        scanner.close();
+
+    }
+
+
+}
---
+++ src/test/java/com/example/TripCostCalculatorTest.java
@@ -0,0 +1,15 @@
+package com.example;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TripCostCalculatorTest {
+    @Test
+    void testCalculateTotalCost() {
+        double calcualtedTotalCost = TripCostCalculator.calculateTotalCost(400, 1.89, 5);
+        
+        assertEquals(37.8, calcualtedTotalCost);
+    }
+}
